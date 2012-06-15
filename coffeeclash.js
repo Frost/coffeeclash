cd0712442775f21f2f75dc3526e02353970694c5
Martin Frost <frost@ceri.se>
2012-06-15 14:42:38 +0200

    Sorting now works again. It's an ugly hack, but it works. Basically it sets a players 'score' attribute to the number of cups they've had today, and it resets that 'score' every time they have another cup.

--- coffeeclash.js
+++ coffeeclash.js
@@ -4,41 +4,8 @@
 Players = new Meteor.Collection("players");
 
 if (Meteor.is_client) {
-  Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
-  };
 
-  Template.leaderboard.selected_name = function () {
-    var player = Players.findOne(Session.get("selected_player"));
-    return player && player.name;
-  };
-
-  Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
-  };
-
-  Template.leaderboard.events = {
-    'click input.inc': function () {
-      var cup = {consumedAt: new Date()};
-      Players.update(Session.get("selected_player"), {$push: {cups: cup}});
-    }
-  };
-
-  Template.player.events = {
-    'click': function () {
-      Session.set("selected_player", this._id);
-    }
-  };
-
-  Template.playerform.events = {
-    'submit': function () {
-      var name = $("input[name=player_name]");
-      var player = Players.insert({name: name.val(), score: 0});
-      Session.set("selected_player", player);
-    }
-  }
-
-  Handlebars.registerHelper("numberOfCupsToday", function(cups) {
+  var numberOfCupsToday = function(cups) {
     if (cups) {
       var cupsToday = cups.map(function(cup) {
         var now = new Date();
@@ -56,7 +23,44 @@ if (Meteor.is_client) {
     } else { 
       return 0;
     }
-  });
+  };
+
+  Template.leaderboard.players = function () {
+    return Players.find({}, {sort: {score: -1, name: 1}});
+  };
+
+  Template.leaderboard.selected_name = function () {
+    var player = Players.findOne(Session.get("selected_player"));
+    return player && player.name;
+  };
+
+  Template.player.selected = function () {
+    return Session.equals("selected_player", this._id) ? "selected" : '';
+  };
+
+  Template.leaderboard.events = {
+    'click input.inc': function () {
+      var cup = {consumedAt: new Date()};
+      var player = Players.findOne(Session.get("selected_player"));
+      Players.update(player._id, {$push: {cups: cup}, $set: {score: numberOfCupsToday(player.cups)}});
+    }
+  };
+
+  Template.player.events = {
+    'click': function () {
+      Session.set("selected_player", this._id);
+    }
+  };
+
+  Template.playerform.events = {
+    'submit': function () {
+      var name = $("input[name=player_name]");
+      var player = Players.insert({name: name.val(), score: 0, cups: []});
+      Session.set("selected_player", player);
+    }
+  }
+
+  Handlebars.registerHelper("numberOfCupsToday", numberOfCupsToday);
 }
 
 // On server startup, create some players if the database is empty.
